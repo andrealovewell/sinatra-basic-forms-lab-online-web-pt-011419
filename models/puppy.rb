26070e26d0c669e9d6ce9223740e4e5d9309948f
Andrea Lovewell <andy.lovewell@gmail.com>
2019-04-26 04:52:40 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,11 +1,10 @@
 class Puppy
+  attr_accessor :name, :breed, :age
 
-  attr_accessor :name, :breed, :months_old
-
-  def initialize(name, breed, months_old)
-    @name=name
-    @breed=breed
-    @months_old=months_old
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
   end
 
   def self.Puppy
