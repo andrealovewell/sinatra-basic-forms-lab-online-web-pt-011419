e6f2f2eaa4b94e106921fbbad612aa00ad8907ab
Andrea Lovewell <andy.lovewell@gmail.com>
2019-04-26 04:55:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,18 +3,18 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-  erb :index
-end
+    erb :index
+  end
 
-get '/new' do
-  erb :create_puppy
-end
+  get '/new' do
+    erb :create_puppy
+  end
 
-post '/new' do
-  @name = params[:name]
-  @breed = params[:breed]
-  @age = params[:age]
-  erb :display_puppy
-end
+  post '/new' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    erb :display_puppy
+  end
 
 end
